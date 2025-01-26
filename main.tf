3613c230a62b120fb87daf8c2718011aed70064d
Shelan Ravin de Livera <shelanrdelivera@gmail.com>
2025-01-26 22:11:17 +0530

    Update main.tf
    LOAD BALANCER

--- main.tf
+++ main.tf
@@ -315,3 +315,80 @@ resource "aws_security_group" "ec2_sg" {
 }
 
 
+# =======================================================
+# LOAD BALANCER
+# =======================================================
+
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group
+# TARGET GROUP
+
+resource "aws_lb_target_group" "app_tg" {
+  name     = "app-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app_vpc.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 2
+  }
+}
+
+# =======================================================
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb
+# LOAD BALANCER
+
+resource "aws_lb" "app_lb" {
+  name               = "app-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = [
+    aws_subnet.public_subnet1.id, 
+    aws_subnet.public_subnet2.id
+    # aws_subnet.private_subnet2.id
+  ]  
+  
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "app_lb"
+  }
+}
+
+# =======================================================
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_listener
+# LISTENER
+
+resource "aws_lb_listener" "app_listener" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+# =======================================================
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group_attachment
+# TARGET GROUP ATTACHMENTS
+
+# Attach EC2 Instances to Target Group
+resource "aws_lb_target_group_attachment" "app_tg_attachment1" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.app_server1.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "app_tg_attachment2" {
+  target_group_arn = aws_lb_target_group.app_tg.arn
+  target_id        = aws_instance.app_server2.id
+  port             = 80
+}
+
+
