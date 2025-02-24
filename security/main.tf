192adfd271f9b79bc329d8deebbf3a32e7987ff4
Shelan Ravin de Livera <shelanrdelivera@gmail.com>
2025-02-25 00:01:37 +0530

    Create security module

---
+++ security/main.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "alb_sg" {
+  name = "application_load_balancer_security_group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ec2_sg" {
+  name = "ec2_security_group"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP"
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
