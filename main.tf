a56d853a0f1d38f74f0c341cbfb250da57e8c45f
Shelan Ravin de Livera <shelanrdelivera@gmail.com>
2025-02-25 10:29:38 +0530

    Prepare for test infrastructure deployment

--- main.tf
+++ main.tf
@@ -58,86 +58,86 @@ module "loadbalancer" {
   ]
 }
 
-# CodeDeploy
-resource "aws_codedeploy_app" "flask_app" {
-  name = "flask_app"
-  compute_platform = "Server"
-}
+# # CodeDeploy
+# resource "aws_codedeploy_app" "flask_app" {
+#   name = "flask_app"
+#   compute_platform = "Server"
+# }
 
-resource "aws_codedeploy_deployment_group" "my_deployment_group" {
-  app_name              = aws_codedeploy_app.flask_app.name
-  deployment_group_name = "MyFlaskAppDeploymentGroup"
-  service_role_arn      = aws_iam_role.codedeploy_role.arn
+# resource "aws_codedeploy_deployment_group" "my_deployment_group" {
+#   app_name              = aws_codedeploy_app.flask_app.name
+#   deployment_group_name = "MyFlaskAppDeploymentGroup"
+#   service_role_arn      = aws_iam_role.codedeploy_role.arn
 
-  deployment_style {
-    deployment_type   = "IN_PLACE"
-    deployment_option = "WITH_TRAFFIC_CONTROL"
-  }
+#   deployment_style {
+#     deployment_type   = "IN_PLACE"
+#     deployment_option = "WITH_TRAFFIC_CONTROL"
+#   }
 
-  load_balancer_info {
-    elb_info {
-      name = aws_lb.app_lb.name
-    }
-    target_group_info {
-      name = aws_lb_target_group.app_tg.name
-    }
-  }
+#   load_balancer_info {
+#     elb_info {
+#       name = aws_lb.app_lb.name
+#     }
+#     target_group_info {
+#       name = aws_lb_target_group.app_tg.name
+#     }
+#   }
 
-  ec2_tag_set {
-    ec2_tag_filter {
-      key   = "Name"
-      value = "MyFlaskAppInstance"
-      type  = "KEY_AND_VALUE"
-    }
-  }
+#   ec2_tag_set {
+#     ec2_tag_filter {
+#       key   = "Name"
+#       value = "MyFlaskAppInstance"
+#       type  = "KEY_AND_VALUE"
+#     }
+#   }
 
-  alarm_configuration {
-    alarms  = ["my-alarm-name"]
-    enabled = true
-  }
+#   alarm_configuration {
+#     alarms  = ["my-alarm-name"]
+#     enabled = true
+#   }
 
-  outdated_instances_strategy = "UPDATE"
-}
+#   outdated_instances_strategy = "UPDATE"
+# }
 
-# CodePipeline
-resource "aws_codepipeline" "flask_pipeline" {
-  name     = "flask-pipeline"
-  role_arn = aws_iam_role.codepipeline_role.arn
+# # CodePipeline
+# resource "aws_codepipeline" "flask_pipeline" {
+#   name     = "flask-pipeline"
+#   role_arn = aws_iam_role.codepipeline_role.arn
 
-  artifact_store {
-    location = "terraform-state-and-file-bucket"
-    type     = "S3"
-  }
+#   artifact_store {
+#     location = "terraform-state-and-file-bucket"
+#     type     = "S3"
+#   }
 
-  stage {
-    name = "Source"
-    action {
-      name             = "SourceAction"
-      category         = "Source"
-      owner            = "AWS"
-      provider         = "S3"
-      version          = "1"
-      output_artifacts = ["source_output"]
-      configuration = {
-        S3Bucket    = "terraform-state-and-file-bucket"
-        S3ObjectKey = "my-flask-app.zip"
-      }
-    }
-  }
+#   stage {
+#     name = "Source"
+#     action {
+#       name             = "SourceAction"
+#       category         = "Source"
+#       owner            = "AWS"
+#       provider         = "S3"
+#       version          = "1"
+#       output_artifacts = ["source_output"]
+#       configuration = {
+#         S3Bucket    = "terraform-state-and-file-bucket"
+#         S3ObjectKey = "my-flask-app.zip"
+#       }
+#     }
+#   }
 
-  stage {
-    name = "Deploy"
-    action {
-      name             = "DeployAction"
-      category         = "Deploy"
-      owner            = "AWS"
-      provider         = "CodeDeploy"
-      version          = "1"
-      input_artifacts  = ["source_output"]
-      configuration = {
-        ApplicationName     = aws_codedeploy_app.flask_app.name
-        DeploymentGroupName = aws_codedeploy_deployment_group.my_deployment_group.deployment_group_name
-      }
-    }
-  }
-}
+#   stage {
+#     name = "Deploy"
+#     action {
+#       name             = "DeployAction"
+#       category         = "Deploy"
+#       owner            = "AWS"
+#       provider         = "CodeDeploy"
+#       version          = "1"
+#       input_artifacts  = ["source_output"]
+#       configuration = {
+#         ApplicationName     = aws_codedeploy_app.flask_app.name
+#         DeploymentGroupName = aws_codedeploy_deployment_group.my_deployment_group.deployment_group_name
+#       }
+#     }
+#   }
+# }
